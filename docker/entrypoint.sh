64c39c0b93158fc5b9f63b68ff991756d8f4840d
Yuce Tekol <yuce.tekol@hazelcast.com>
2021-08-18 16:29:32 +0300

    updates

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,20 @@
 #! /bin/bash
 
+set -e
+set -u
+
+run_test () {
+  sleep 1
+  # assumes /app is the project root
+  su -s /bin/bash hz -c "
+    bash rc.sh start
+    sleep 5
+    source venv/bin/activate
+    cd ~/app
+    make test-all
+  "
+}
+
 principal="hz/${HOSTNAME}@EXAMPLE.COM"
 
 echo "$(hostname -I) example.com" >> /etc/hosts
@@ -11,4 +26,20 @@ chmod 777 /etc/krb5.keytab
 
 service krb5-kdc restart
 
-su -s /bin/bash hz
+cmd=${1:-}
+
+echo "CMD: $cmd"
+
+case "$cmd" in
+  "")
+  /bin/bash
+  ;;
+
+  "test")
+  run_test
+  ;;
+
+  "*")
+  /bin/bash -c "$@"
+  ;;
+esac
