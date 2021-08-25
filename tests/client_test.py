0a707f71659ace935758f2060a3cbdf8ea69ffd7
Yuce Tekol <yuce.tekol@hazelcast.com>
2021-08-25 09:40:48 +0300

    client timeout and logs in tests

--- tests/client_test.py
+++ tests/client_test.py
@@ -1,9 +1,23 @@
+import logging
 import unittest
 
 import hazelcast
 
 import hzkerberos
 from .util import default_keytab, Address
+
+
+def customize_logger(name):
+    lg = logging.getLogger(name)
+    lg.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    handler.setFormatter(formatter)
+    lg.addHandler(handler)
+
+
+customize_logger("hazelcast")
+customize_logger("hzkerberos")
 
 
 class ClientTest(unittest.TestCase):
@@ -16,7 +30,11 @@ class ClientTest(unittest.TestCase):
     def test_token(self):
         token_provider = hzkerberos.TokenProvider(keytab=default_keytab())
         addr = "%s:5701" % self.default_address.host
-        client = hazelcast.HazelcastClient(cluster_members=[addr], token_provider=token_provider)
+        client = hazelcast.HazelcastClient(
+            cluster_members=[addr],
+            token_provider=token_provider,
+            cluster_connect_timeout=5,
+        )
         m = client.get_map("auth-map").blocking()
         m.put("k1", "v1")
         v = m.get("k1")
