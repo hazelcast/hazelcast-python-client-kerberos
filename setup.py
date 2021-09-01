008ca17cba0a589d8ce5553d4bc9c0fc3e3bd709
Yuce Tekol <yuce.tekol@hazelcast.com>
2021-09-01 20:51:39 +0300

    updates

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     install_requires=["gssapi"],
     tests_require=["hazelcast-python-client", "nose", "thrift"],
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
